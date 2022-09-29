0e059d548055d65a5089308a7e95526636246783
arijeet <arijeet@sbox.ltd>
2022-09-29 20:01:07 +0530

    search bar added

--- lib/screens/crypto_screen.dart
+++ lib/screens/crypto_screen.dart
@@ -18,8 +18,52 @@ class _CryptoCurrencyScreenState extends State<CryptoCurrencyScreen> {
             Icons.sync,
           ),
         ),
-        body: const Center(
-          child: Text('data'),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    flex: 8,
+                    child: Container(
+                      height: 60,
+                      padding: const EdgeInsets.all(8),
+                      child: Stack(
+                        alignment: Alignment.centerRight,
+                        children: [
+                          TextField(
+                            onChanged: (value) {
+                              // customersSearchString = value;
+                            },
+                            decoration: InputDecoration(
+                              fillColor: Colors.black.withOpacity(0.1),
+                              filled: true,
+                              hintText: 'Enter currency pair',
+                              hintStyle:
+                                  const TextStyle(fontWeight: FontWeight.w400),
+                              border: const OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                              ),
+                              contentPadding: const EdgeInsets.all(10),
+                            ),
+                          ),
+                          Positioned(
+                            right: 10,
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.search),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
